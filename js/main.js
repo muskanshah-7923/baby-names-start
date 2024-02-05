7fa6d76c851c1d5a3206a60d5e49a093d9261749
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2024-02-05 13:06:00 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// Baby Names Start
+// 2020 Alberta Baby Names
+// https://open.alberta.ca/opendata/frequency-and-ranking-of-baby-names-by-year-and-gender
+
+// Variables for html elements
+let goBtn = document.getElementById("go-btn");
+let menuSelect = document.getElementById("menu-select");
+let container = document.getElementById("container");
+let nameCountSpan = document.getElementById("name-count");
+
+// Initialize Array of Baby Names from Text File
+let babyNames = await loadTextFile("data/baby-names.txt");
+
+// Event Listener on Go Button
+goBtn.addEventListener("click", goBtnClicked);
+
+// Process Go Button Click
+function goBtnClicked() {
+  // Get Menu Selection
+  let selection = menuSelect.value;
+
+  // Process Menu Selection
+  if (selection === "display-all") {
+    displayAll();
+  } else if (selection === "starting-letter") {
+    searchStartingLetter();
+  } else if (selection === "exact-length") {
+    searchExactLength();
+  } else if (selection === "range-length") {
+    searchRangeLength();
+  }
+}
+
+// Display All Characters
+function displayAll() {
+  container.innerHTML = "Display All";
+}
+
+// Display Names with Starting Letter
+function searchStartingLetter() {
+  container.innerHTML = "Display Starting Letter";
+}
+
+// Display Names with Exact Length
+function searchExactLength() {
+  container.innerHTML = "Display Exact Length";
+}
+
+// Display Names within a Range of Lengths
+function searchRangeLength() {
+  container.innerHTML = "Display Range Length";
+}
